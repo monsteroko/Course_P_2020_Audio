c718d6d207aab7c65c257efa75441272fbb2e33f
Oleg Oleynik <oko241999@gmail.com>
2020-05-06 02:07:19 +0300

    Добавьте файлы проекта.

---
+++ Course_P_2020_Audio/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Course_P_2020_Audio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.редактированиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.конвертироватьВWAVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.обАвтореToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.waveViewer1 = new NAudio.Gui.WaveViewer();
+            this.питчшифтингToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.редактированиеToolStripMenuItem,
+            this.обАвтореToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // редактированиеToolStripMenuItem
+            // 
+            this.редактированиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.конвертироватьВWAVToolStripMenuItem,
+            this.питчшифтингToolStripMenuItem});
+            this.редактированиеToolStripMenuItem.Name = "редактированиеToolStripMenuItem";
+            this.редактированиеToolStripMenuItem.Size = new System.Drawing.Size(137, 24);
+            this.редактированиеToolStripMenuItem.Text = "Редактирование";
+            // 
+            // конвертироватьВWAVToolStripMenuItem
+            // 
+            this.конвертироватьВWAVToolStripMenuItem.Enabled = false;
+            this.конвертироватьВWAVToolStripMenuItem.Name = "конвертироватьВWAVToolStripMenuItem";
+            this.конвертироватьВWAVToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.конвертироватьВWAVToolStripMenuItem.Text = "Конвертировать в WAV";
+            this.конвертироватьВWAVToolStripMenuItem.Click += new System.EventHandler(this.конвертироватьВWAVToolStripMenuItem_Click);
+            // 
+            // обАвтореToolStripMenuItem
+            // 
+            this.обАвтореToolStripMenuItem.Name = "обАвтореToolStripMenuItem";
+            this.обАвтореToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.обАвтореToolStripMenuItem.Text = "Об авторе";
+            this.обАвтореToolStripMenuItem.Click += new System.EventHandler(this.обАвтореToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(449, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(359, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Pause";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // waveViewer1
+            // 
+            this.waveViewer1.Location = new System.Drawing.Point(38, 199);
+            this.waveViewer1.Name = "waveViewer1";
+            this.waveViewer1.SamplesPerPixel = 128;
+            this.waveViewer1.Size = new System.Drawing.Size(737, 97);
+            this.waveViewer1.StartPosition = ((long)(0));
+            this.waveViewer1.TabIndex = 5;
+            this.waveViewer1.WaveStream = null;
+            // 
+            // питчшифтингToolStripMenuItem
+            // 
+            this.питчшифтингToolStripMenuItem.Enabled = false;
+            this.питчшифтингToolStripMenuItem.Name = "питчшифтингToolStripMenuItem";
+            this.питчшифтингToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.питчшифтингToolStripMenuItem.Text = "Питч-шифтинг";
+            this.питчшифтингToolStripMenuItem.Click += new System.EventHandler(this.питчшифтингToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 308);
+            this.Controls.Add(this.waveViewer1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Работа с аудиофайлом";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem обАвтореToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem редактированиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem конвертироватьВWAVToolStripMenuItem;
+        private NAudio.Gui.WaveViewer waveViewer1;
+        private System.Windows.Forms.ToolStripMenuItem питчшифтингToolStripMenuItem;
+    }
+}
+
---
+++ Course_P_2020_Audio/Form1.cs
@@ -0,0 +1,149 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave.SampleProviders;
+using System.Threading;
+
+namespace Course_P_2020_Audio
+{
+
+    public partial class Form1 : Form
+    {
+        string rfname = @"C:\r.mp3";
+        WaveOutEvent outputDevice;
+        AudioFileReader audioFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void обАвтореToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Олейник О.К., 535а", "About", MessageBoxButtons.OK);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.InitialDirectory = "C:\\";
+            open.Filter = "mp3 files (*.mp3)|*.mp3|wav files (*.wav)|*.wav";
+            open.FilterIndex = 1;
+            open.Title = "Открыть файл";
+            try
+            {
+                if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    rfname = open.FileName;
+                }
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("Не выбран файл!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            label1.Text = "Открыт файл: " + rfname;
+            if (rfname.IndexOf(".mp3") == -1)
+            {
+                конвертироватьВWAVToolStripMenuItem.Enabled = false;
+                waveViewer1.WaveStream = new WaveFileReader(rfname);
+            }
+            else
+            {
+                конвертироватьВWAVToolStripMenuItem.Enabled = true;
+            }
+            питчшифтингToolStripMenuItem.Enabled = true;
+            label1.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(rfname);
+                outputDevice.Init(audioFile);
+            }
+            outputDevice.Play();
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            outputDevice.Dispose();
+            outputDevice = null;
+            audioFile.Dispose();
+            audioFile = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            outputDevice?.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            outputDevice?.Pause();
+        }
+
+        private void конвертироватьВWAVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = "C:\\";
+            save.Filter = "wav files (*.wav)|*.wav";
+            save.FilterIndex = 1;
+            save.Title = "Сохранить файл";
+            if (save.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = save.FileName;
+            using (var reader = new Mp3FileReader(rfname))
+            {
+                WaveFileWriter.CreateWaveFile(filename, reader);
+            }
+            MessageBox.Show("Файл сохранен!");
+        }
+
+        private void питчшифтингToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var inPath = rfname;
+            var semitone = Math.Pow(2, 1.0 / 12);
+            var upOneTone = semitone * semitone;
+            var downOneTone = 1.0 / upOneTone;
+            using (var reader = new MediaFoundationReader(inPath))
+            {
+                var pitch = new SmbPitchShiftingSampleProvider(reader.ToSampleProvider());
+                using (var device = new WaveOutEvent())
+                {
+                    pitch.PitchFactor = (float)downOneTone; // or downOneTone
+                                                          // just playing the first 10 seconds of the file
+                    SaveFileDialog save = new SaveFileDialog();
+                    save.InitialDirectory = "C:\\";
+                    save.Filter = "wav files (*.wav)|*.wav";
+                    save.FilterIndex = 1;
+                    save.Title = "Сохранить файл";
+                    if (save.ShowDialog() == DialogResult.Cancel)
+                        return;
+                    string filename = save.FileName;
+                    WaveFileWriter.CreateWaveFile(filename, pitch.ToWaveProvider16());
+                    MessageBox.Show("Файл сохранен!");
+                }
+            }
+        }
+    }
+}
